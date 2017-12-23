b16c16d62e357c8665c61522c1f7ee822d3f4483
Hugo Moragues <hugo.moragues@gmail.com>
2017-12-23 18:59:31 +0100

    some parts

---
+++ src/components/buttons.ts
@@ -0,0 +1,30 @@
+namespace VendingMachine {
+	export const buttonPressEventName = 'button-clicked';
+	export interface ButtonPressEventDetails {
+		buttonValue: string;
+	}
+
+	export class Button extends Component {
+		private buttonElement: Element;
+		
+		constructor(private label: string) {
+			super();
+		}
+
+		protected renderSelf(element: Element): Element {
+			let newElement = document.createElement('button');
+			newElement.classList.add('button');
+			newElement.classList.add(`button-${this.label.toLowerCase()}`);
+			newElement.textContent = this.label;
+			element.appendChild(newElement);
+			this.buttonElement = newElement;
+			newElement.addEventListener('click', () => {
+				this.raiseEvent(buttonPressEventName, {
+					buttonValue: this.label
+				});
+			});
+
+			return newElement;
+		}
+	}
+}
