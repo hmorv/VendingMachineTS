0406c7830c9856bfb7eb10d88aa6a797aaee478d
Hugo Moragues <hugo.moragues@gmail.com>
2018-01-05 23:25:01 +0100

    HG

---
+++ src/helpers/slotCodeGenerator.ts
@@ -0,0 +1,11 @@
+namespace VendingMachine.Helpers {
+	export class SlotCodeGenerator {
+		public static generate(index: number): string {
+			let code = '';
+			let indexArray = index.toString().split('');
+			indexArray.forEach(unit => code += regularKeyValuesArray[parseInt(unit)]);
+
+			return code;
+		}
+	}
+}
